6e68c0a1d01dec4bc95d6ba9fe65a4a61f454520
synthresin <synthresin@me.com>
2012-10-25 19:57:59 +0900

    ajax 통신 성공

--- js/SUStorySlide.js
+++ js/SUStorySlide.js
@@ -1,13 +1,16 @@
 SUStorySlide = (function($) {
 	function SUStorySlide(params) {
 		this.view = params.view;
+		this.ajax_url = params.url;
 		this.main_object = params.view.find('.main-object');
 		this.objects = [];
+		
 	}
 	
 	SUStorySlide.prototype = {
 		init: function() {
 			console.log(this.view.attr('class') + ' ready!');
+			this.parse_data();
 		},
 
 		activate: function() {
@@ -19,6 +22,18 @@ SUStorySlide = (function($) {
 		deactivate: function() {
 			console.log(this.view.attr('class') + ' deactivated!');
 			this.view.hide();
+		},
+
+		parse_data: function() {
+			$.ajax({
+				url: 'http://sunshineunderground.kr/sample.json',
+				dataType: 'json',
+				type: 'GET',
+				success: this.request_success
+			});
+		},
+		request_success: function(data, textStatus, jqXHR) {
+			console.log(data.length);
 		},
 
 		repeatFunc: function(elem) {
